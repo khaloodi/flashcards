c2c0e9ea4f8973fb5ac2a7739c44446069f99451
Khaled Adad <khaledadad@lewisu.edu>
2021-04-14 19:26:03 -0500

    wip

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const express = require('express')
 // app is the central part of our application, we will extend it with routes, middleware, and other settings
 const app = express();
 
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
+];
+
 // tells express which template engine to use, by default templates are saved in a folder called views
 app.set('view engine', 'pug')
 
@@ -18,6 +27,13 @@ app.get('/', (req, res) => {
 app.get('/cards', (req, res) => {
     //call back function for when client requests this route
     res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is?" });
+    // res.locals.prompt = "Who is buried in Grant's tomb?"
+    // res.render('card');
+});
+
+app.get('/sandbox', (req, res) => {
+    //call back function for when client requests this route
+    res.render('sandbox', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is?", colors });
     // res.locals.prompt = "Who is buried in Grant's tomb?"
     // res.render('card');
 });
