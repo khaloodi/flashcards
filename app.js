529910125e3db2e93bcf9982017632dc6d872480
Khaled Adad <khaledadad@lewisu.edu>
2021-04-30 11:44:29 -0500

    Added error template and testing errors

--- app.js
+++ app.js
@@ -26,13 +26,20 @@ app.set('view engine', 'pug')
 // often we pass middleware as an anonymous function to the app use method
 // middleware runs every time a request comes through the app
 app.use((req, res, next) => {
-    req.message = 'This message made it' //message is not a special name, can call anything or add more properties if wanted
-    next() // passing control forward throughout the app
+    // req.message = 'This message made it' //message is not a special name, can call anything or add more properties if wanted
+    console.log('Hello')
+    const err = new Error('Oh noes')
+    err.status = 500
+    next(err) // passing control forward throughout the app, signals the end of middleware functions
+        // the app will "hang" if middleware doesn't end with a next method
+        // with the get methods we don't need next because they are sending a response to the user
+        // such as json res.send or res.render
 })
 
 app.use((req, res, next) => {
-    console.log(req.message)
-    next() // passing control forward throughout the app
+    console.log('world')
+        // console.log(req.message)
+    next() // passing control forward throughout the app signals the end of middleware functions
 })
 
 // root route
@@ -82,6 +89,14 @@ app.post('/goodbye', (req, res) => {
     res.clearCookie('username')
     res.redirect('/hello')
 });
+
+app.use((err, req, res, next) => {
+    // res.locals.error = err   .... if I use this line with locals I don't need the err object below
+    res.locals.error = err
+    res.status(500) // causes the error code to pop up which is not something baked into javascripts error object
+    res.render('error')
+        //giving the template access to the error data via err object with res.render('error', err) doesn't work for some reason
+})
 
 // setup developmentserver
 // takes port number as parameter
