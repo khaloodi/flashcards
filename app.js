2d50fb7c24553168cf4c7975419c664bae76ab94
Khaled Adad <khaledadad@lewisu.edu>
2021-05-13 10:29:47 -0500

    added public styles

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser')
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(express.static('public'))
 
 const colors = [
     'red',
